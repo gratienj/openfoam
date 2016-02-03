8bb163531279274019a1d239a73622c67ec3fa61
Henry Weller <http://cfd.direct>
2016-02-03 21:24:38 +0000

    foamyHexMeshBackgroundMesh: Updated to compile

--- applications/utilities/mesh/generation/foamyMesh/foamyHexMeshBackgroundMesh/foamyHexMeshBackgroundMesh.C
+++ applications/utilities/mesh/generation/foamyMesh/foamyHexMeshBackgroundMesh/foamyHexMeshBackgroundMesh.C
@@ -2,7 +2,7 @@
  =========                   |
  \\      /   F ield          | OpenFOAM: The Open Source CFD Toolbox
   \\    /    O peration      |
-   \\  /     A nd            | Copyright (C) 2012-2015 OpenFOAM Foundation
+   \\  /     A nd            | Copyright (C) 2012-2016 OpenFOAM Foundation
     \\/      M anipulation   |
 -------------------------------------------------------------------------------
 License
@@ -576,7 +576,7 @@ int main(int argc, char *argv[])
         runTime,
         rndGen,
         geometryToConformTo,
-        backgroundMeshDict
+        foamyHexMeshDict
     );
 
     if (writeMesh)
