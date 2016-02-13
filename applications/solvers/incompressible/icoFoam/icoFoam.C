0124aad70bf148e88a8b7afca78f8062df6fe0e6
Henry Weller <http://cfd.direct>
2016-02-13 17:48:26 +0000

    Solvers: Added support for extrapolated pressure boundary conditions
    The boundary conditions of HbyA are now constrained by the new "constrainHbyA"
    function which applies the velocity boundary values for patches for which the
    velocity cannot be modified by assignment and pressure extrapolation is
    not specified via the new
    "fixedFluxExtrapolatedPressureFvPatchScalarField".
    
    The new function "constrainPressure" sets the pressure gradient
    appropriately for "fixedFluxPressureFvPatchScalarField" and
    "fixedFluxExtrapolatedPressureFvPatchScalarField" boundary conditions to
    ensure the evaluated flux corresponds to the known velocity values at
    the boundary.
    
    The "fixedFluxPressureFvPatchScalarField" boundary condition operates
    exactly as before, ensuring the correct flux at fixed-flux boundaries by
    compensating for the body forces (gravity in particular) with the
    pressure gradient.
    
    The new "fixedFluxExtrapolatedPressureFvPatchScalarField" boundary
    condition may be used for cases with or without body-forces to set the
    pressure gradient to compensate not only for the body-force but also the
    extrapolated "HbyA" which provides a second-order boundary condition for
    pressure.  This is useful for a range a problems including impinging
    flow, extrapolated inlet conditions with body-forces or for highly
    viscous flows, pressure-induced separation etc.  To test this boundary
    condition at walls in the motorBike tutorial case set
    
        lowerWall
        {
            type            fixedFluxExtrapolatedPressure;
        }
    
        motorBikeGroup
        {
            type            fixedFluxExtrapolatedPressure;
        }
    
    Currently the new extrapolated pressure boundary condition is supported
    for all incompressible and sub-sonic compressible solvers except those
    providing implicit and tensorial porosity support.  The approach will be
    extended to cover these solvers and options in the future.
    
    Note: the extrapolated pressure boundary condition is experimental and
    requires further testing to assess the range of applicability,
    stability, accuracy etc.
    
    Henry G. Weller
    CFD Direct Ltd.

--- applications/solvers/incompressible/icoFoam/icoFoam.C
+++ applications/solvers/incompressible/icoFoam/icoFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -104,9 +104,7 @@ int main(int argc, char *argv[])
         while (piso.correct())
         {
             volScalarField rAU(1.0/UEqn.A());
-
-            volVectorField HbyA("HbyA", U);
-            HbyA = rAU*UEqn.H();
+            volVectorField HbyA(constrainHbyA(rAU*UEqn.H(), U, p));
             surfaceScalarField phiHbyA
             (
                 "phiHbyA",
@@ -115,6 +113,9 @@ int main(int argc, char *argv[])
             );
 
             adjustPhi(phiHbyA, U, p);
+
+            // Update the pressure BCs to ensure flux consistency
+            constrainPressure(p, U, phiHbyA, rAU);
 
             // Non-orthogonal pressure corrector loop
             while (piso.correctNonOrthogonal())
