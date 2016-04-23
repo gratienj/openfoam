b527763b795894e8e2e3b10d637fdd8aa25eba8d
Henry Weller <http://cfd.direct>
2016-04-23 10:04:39 +0100

    fireFoam: Added optional hydrostatic initialization of the pressure and density
    Also added the new prghTotalHydrostaticPressure p_rgh BC which uses the
    hydrostatic pressure field as the reference state for the far-field
    which provides much more accurate entrainment is large open domains
    typical of many fire simulations.
    
    The hydrostatic field solution is controlled by the optional entries in
    the fvSolution.PIMPLE dictionary, e.g.
    
        hydrostaticInitialization yes;
        nHydrostaticCorrectors 5;
    
    and the solver must also be specified for the hydrostatic p_rgh field
    ph_rgh e.g.
    
        ph_rgh
        {
            $p_rgh;
        }
    
    Suitable boundary conditions for ph_rgh cannot always be derived from
    those for p_rgh and so the ph_rgh is read to provide them.
    
    To avoid accuracy issues with IO, restart and post-processing the p_rgh
    and ph_rgh the option to specify a suitable reference pressure is
    provided via the optional pRef file in the constant directory, e.g.
    
        dimensions      [1 -1 -2 0 0 0 0];
        value           101325;
    
    which is used in the relationship between p_rgh and p:
    
        p = p_rgh + rho*gh + pRef;
    
    Note that if pRef is specified all pressure BC specifications in the
    p_rgh and ph_rgh files are relative to the reference to avoid round-off
    errors.
    
    For examples of suitable BCs for p_rgh and ph_rgh for a range of
    fireFoam cases please study the tutorials in
    tutorials/combustion/fireFoam/les which have all been updated.
    
    Henry G. Weller
    CFD Direct Ltd.

--- applications/solvers/combustion/fireFoam/fireFoam.C
+++ applications/solvers/combustion/fireFoam/fireFoam.C
@@ -57,7 +57,6 @@ int main(int argc, char *argv[])
     pimpleControl pimple(mesh);
 
     #include "createFields.H"
-    #include "createMRF.H"
     #include "createFvOptions.H"
     #include "createClouds.H"
     #include "createSurfaceFilmModel.H"
