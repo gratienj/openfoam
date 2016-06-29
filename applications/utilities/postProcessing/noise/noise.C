316c281a2ae3aca6367cc34e25136450fceea495
Andrew Heather <a.heather@opencfd.co.uk>
2016-06-29 20:46:20 +0100

    ENH: Noise functionality library and application updates

--- applications/utilities/postProcessing/noise/noise.C
+++ applications/utilities/postProcessing/noise/noise.C
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
     #include "setRootCase.H"
     #include "createTime.H"
 
-    word dictName("noiseDict");
+    fileName dictName(runTime.system()/"noiseDict");
     if (args.optionFound("dict"))
     {
         dictName = args["dict"];
@@ -119,7 +119,6 @@ int main(int argc, char *argv[])
         IOobject
         (
             dictName,
-            runTime.system(),
             runTime,
             IOobject::MUST_READ
         )
