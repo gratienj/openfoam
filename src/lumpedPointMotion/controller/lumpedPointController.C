3ef8abaa2ebb46b5ac593051862475c4cc81d7ba
Mark Olesen <Mark.Olesen@esi-group.com>
2019-10-04 15:33:02 +0200

    WIP: lumped point motion using local linear basic functions (#1341)

---
+++ src/lumpedPointMotion/controller/lumpedPointController.C
@@ -0,0 +1,91 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2019 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "lumpedPointController.H"
+#include "dictionary.H"
+#include "Map.H"
+#include "labelField.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::lumpedPointController::lumpedPointController()
+:
+    pointLabels_()
+{}
+
+
+Foam::lumpedPointController::lumpedPointController
+(
+    const labelUList& pointLabels
+)
+:
+    pointLabels_(pointLabels)
+{}
+
+
+Foam::lumpedPointController::lumpedPointController
+(
+    labelList&& pointLabels
+)
+:
+    pointLabels_(std::move(pointLabels))
+{}
+
+
+Foam::lumpedPointController::lumpedPointController
+(
+    const dictionary& dict
+)
+:
+    pointLabels_(dict.get<labelList>("pointLabels"))
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::lumpedPointController::remapPointLabels
+(
+    const label nPoints,
+    const Map<label>& originalIds
+)
+{
+    if (originalIds.size())
+    {
+        for (label& pointi : pointLabels_)
+        {
+            pointi = originalIds[pointi];
+        }
+    }
+
+    if (min(pointLabels_) < 0 || max(pointLabels_) >= nPoints)
+    {
+        FatalErrorInFunction
+            << "Point id out-of-range: " << flatOutput(pointLabels_) << nl
+            << exit(FatalError);
+    }
+}
+
+
+// ************************************************************************* //
