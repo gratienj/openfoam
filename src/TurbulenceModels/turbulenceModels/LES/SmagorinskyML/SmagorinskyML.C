19378c5ade86cbcfd2e5c675e798fa47f09f1932
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2024-10-10 13:58:50 +0200

    plug ML4Tub library

--- src/TurbulenceModels/turbulenceModels/LES/SmagorinskyML/SmagorinskyML.C
+++ src/TurbulenceModels/turbulenceModels/LES/SmagorinskyML/SmagorinskyML.C
@@ -28,6 +28,7 @@ License
 
 #include "SmagorinskyML.H"
 #include "fvOptions.H"
+#include "IOstreams.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -135,6 +136,16 @@ SmagorinskyML<BasicTurbulenceModel>::SmagorinskyML
     {
         this->printCoeffs(type);
     }
+#ifdef USE_ML4TURB
+    ml4turb_enabled_ = this->LESDict_.template getOrDefault<Switch>("ml4turb",false);
+    if(ml4turb_enabled_)
+    {
+      ml4trub_model_ = this->LESDict_.template getOrDefault<word>("MLModelConfig","ml4turb-config.json") ;
+      clip_ = this->LESDict_.template getOrDefault<double>("clip",0.) ;
+      std::cout<<"ML4TURB INFO : MODEL PATH "<<ml4trub_model_<<std::endl ;
+      std::cout<<"               CLIP VALUE "<<clip_<<std::endl ;
+    }
+#endif
 }
 
 
@@ -168,21 +179,70 @@ void SmagorinskyML<BasicTurbulenceModel>::correct()
     std::cout<<"SmagorinskyML::correctNut()"<<std::endl;
     correctNut();
 
+#ifdef USE_ML4TURB
+    if(!ml4turb_api_initialized_ && ml4turb_enabled_)
+    {
+      mlturb_api_ptr_.reset(new ml4turb::ML4Turb()) ;
+      mlturb_api_ptr_->initFromConfigFile(ml4trub_model_) ;
+      ml4turb_api_initialized_ = true;
+    }
+#endif
+
     std::cout<<"BUILD T SGS"<<std::endl ;
     volTensorField turb_t((this->alpha_*this->rho_*this->nut())*dev2(T(fvc::grad(this->U_)))) ;
     volSymmTensorField D(symm(fvc::grad(this->U_)));
+    std::size_t icount = 0;
+#ifdef USE_ML4TURB
+    auto nb_cells = this->mesh_.cells().size() ;
+    std::vector<bool> filter(nb_cells) ;
+    double mag_d_max = 0. ;
+    if(ml4turb_enabled_)
+    {
+      mlturb_api_ptr_->startCompute(nb_cells) ;
+      forAll(TSGS_, celli)
+      {
+          const symmTensor& d = D[celli];
+          double mag_d = mag(d) ;
+          mag_d_max = std::max(mag_d_max,mag_d) ;
+          if(mag_d>clip_)
+          {
+            std::vector<float> grad_vit = {  d.xx(), d.xy(), d.xz(),
+                                             d.yx(), d.yy(), d.yz(),
+                                             d.xz(), d.yz(), d.zz() };
+            mlturb_api_ptr_->asynchCompute(grad_vit) ;
+            filter[icount] = true ;
+          }
+          else
+            filter[icount] = false ;
+          ++icount ;
+      }
+      mlturb_api_ptr_->endCompute() ;
+    }
+    std::cout<<"MAG D MAX : "<<mag_d_max<<std::endl ;
+    std::vector<float> tau_ij(6) ;
+    icount = 0 ;
+ #endif
+
     forAll(TSGS_, celli)
     {
-        const symmTensor& d = D[celli];
-        symmTensor& tsgs    = TSGS_[celli];
-        auto& t             = turb_t[celli] ;
-        //std::cout<<"||D["<<celli<<"]|| = "<<mag(d)<<std::endl ;
-        //std::cout<<"turb_t["<<celli<<"]="<<typeid(t).name()<<" "<<t.xx()<<" "<<t.yy()<<" "<<t.zz()<<std::endl ;
+      symmTensor& tsgs = TSGS_[celli];
+#ifdef USE_ML4TURB
+      if(filter[icount] && ml4turb_enabled_)
+      {
+        mlturb_api_ptr_->getNextResult(tau_ij) ;
+        tsgs = symmTensor(tau_ij[0],tau_ij[1],tau_ij[2],
+                                    tau_ij[3],tau_ij[4],
+                                              tau_ij[5]) ;
+      }
+      else
+#endif
+      {
+        auto& t = turb_t[celli] ;
         tsgs = symmTensor(t.xx(),t.xy(),t.xz(),
                                  t.yy(),t.yz(),
                                         t.zz()) ;
-        //std::cout<<"Tensor D["<<celli<<"] = "<<d.xx()<<" "<<d.yy()<<" "<<d.zz()<<std::endl ;
-        //std::cout<<"Tensor TSGS["<<celli<<"] = "<<tsgs.xx()<<" "<<tsgs.yy()<<" "<<tsgs.zz()<<std::endl ;
+      }
+      ++icount ;
     }
     std::cout<<"FIN SmagorinskyML::correct()"<<typeid(*this).name()<<std::endl;
 }
