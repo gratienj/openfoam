885f3c5b55c79b393e0247f37319c92c8fadc16c
Kutalmis Bercin <kutalmis.bercin@keysight.com>
2026-01-07 14:00:32 +0000

    ENH: add Generalized k-omega two-equation turbulence model (GEKO)

--- src/TurbulenceModels/compressible/turbulentFluidThermoModels/turbulentFluidThermoModels.C
+++ src/TurbulenceModels/compressible/turbulentFluidThermoModels/turbulentFluidThermoModels.C
@@ -7,6 +7,7 @@
 -------------------------------------------------------------------------------
     Copyright (C) 2013-2016 OpenFOAM Foundation
     Copyright (C) 2022-2023 OpenCFD Ltd.
+    Copyright (C) 2026 Keysight Technologies
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -111,6 +112,8 @@ makeRASModel(SSG);
 #include "EBRSM.H"
 makeRASModel(EBRSM);
 
+#include "GEKO.H"
+makeRASModel(GEKO);
 
 // -------------------------------------------------------------------------- //
 // LES models
