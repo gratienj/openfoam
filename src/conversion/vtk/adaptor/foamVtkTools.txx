ROCm/OpenFOAM_HMM

commit a0d79333608624885837d818161794d4f3b2fd0c

ENH: support vtu mesh subsetting, creation from cellShapes
- support simple mesh subsetting to vtu formats to enable debug output
  for a subsection of an existing polyMesh

- rudimentary support for VTK from cellShapes is intended for handling
  basic primitive cell shapes without a full blown polyMesh description.

  For example,

     // Create an empty polyMesh with points only
     polyMesh debugMesh
     (
         io,
         std::move(points),
         faceList(),     // faces
         labelList(),    // owner
         labelList(),    // neighbour
         true            // syncPar
     );

    // Establish the appropriate VTK sizing for the TET shapes
    vtk::vtuCells vtuCells;
    vtuCells.resetShapes(debugCutTets);
    vtuCells.nPoints(debugMesh.nPoints());

  NOTE
  Since the vtk::internalMeshWriter only uses the polyMesh reference
  for the points, it is also possible to create the vtuCells
  description without a pointField (or from a different mesh
  description) and write out the connectivity using the pointField
  from a different mesh.

src/fileFormats/vtk/part/foamVtuCells.H
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2014-2020 OpenCFD Ltd.
+    Copyright (C) 2014-2021 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -59,9 +59,6 @@ SourceFiles
 namespace Foam
 {
 
-// Forward Declarations
-class polyMesh;
-
 namespace vtk
 {
 // Forward Declarations
@@ -110,9 +107,14 @@ class vtuCells
 
     // Private Member Functions
 
-        //- Create the geometry using the previously requested output and
-        //- decomposition types.
-        void repopulate(const polyMesh& mesh);
+        //- Resize storage
+        void resize_all();
+
+        //- Create the geometry entries
+        void populateOutput(const polyMesh& mesh);
+
+        //- Create the geometry entries from shapes
+        void populateOutput(const UList<cellShape>& shapes);
 
         //- No copy construct
         vtuCells(const vtuCells&) = delete;
@@ -162,10 +164,10 @@ public:
     // Access
 
         //- The output content type
-        inline enum contentType content() const;
+        inline enum contentType content() const noexcept;
 
         //- Query the polyhedral decompose requested flag.
-        inline bool decomposeRequested() const;
+        inline bool decomposeRequested() const noexcept;
 
         //- True if no cellTypes are populated.
         inline bool empty() const noexcept;
@@ -183,6 +185,14 @@ public:
         //- decomposition types.
         void reset(const polyMesh& mesh);
 
+        //- Create the geometry for a mesh subset,
+        //- using previously requested output and decomposition types.
+        void reset
+        (
+            const polyMesh& mesh,
+            const labelUList& subsetCellsIds
+        );
+
         //- Respecify requested output and decomposition type prior to
         //- creating the geometry
         void reset
@@ -192,35 +202,42 @@ public:
             const bool decompose
         );
 
+        //- Reset sizing using primitive shapes only (ADVANCED USAGE)
+        //  Effectively removes any polyhedrals!
+        void resetShapes(const UList<cellShape>& shapes);
+
         //- Renumber cell ids to account for subset meshes
         void renumberCells(const labelUList& mapping);
 
         //- Renumber point ids to account for subset meshes
         void renumberPoints(const labelUList& mapping);
 
+        //- Define which additional cell-centres are to be used (ADVANCED!)
+        void addPointCellLabels(const labelUList& cellIds);
+
 
     // Storage Access
 
         //- Values for "types" (XML) and "CELL_TYPES" (legacy)
-        inline const List<uint8_t>& cellTypes() const;
+        inline const List<uint8_t>& cellTypes() const noexcept;
 
         //- Values for "connectivity" (XML) or "CELLS" (legacy)
-        inline const labelList& vertLabels() const;
+        inline const labelList& vertLabels() const noexcept;
 
         //- Values for "offsets" (XML only)
-        inline const labelList& vertOffsets() const;
+        inline const labelList& vertOffsets() const noexcept;
 
         //- Values for "faces" (XML only)
-        inline const labelList& faceLabels() const;
+        inline const labelList& faceLabels() const noexcept;
 
         //- Values for "faceoffset" (XML only)
-        inline const labelList& faceOffsets() const;
+        inline const labelList& faceOffsets() const noexcept;
 
         //- Additional point addressing (from added point to original cell)
-        inline const labelList& addPointCellLabels() const;
+        inline const labelList& addPointCellLabels() const noexcept;
 
         //- Original cell ids for all cells (regular and decomposed).
-        inline const labelList& cellMap() const;
+        inline const labelList& cellMap() const noexcept;
 };
 
 

gratienj/openfoam

commit 1066bb339f660e6a38e6b1f54ffcf7ab62929a00

DEFEATURE: remove old support for old vtkCellArray (#3483)
- support for VTK_CELL_ARRAY_V2 was added to OpenFOAM in Jan-2020,
  can now remove the old (internal1) code as being defunct.

REGRESSION: incorrect addressing for vtk poly-decompose (fixes #3484)

- regression introduced with a0d793336086 (2021-09-17)

src/conversion/vtk/adaptor/foamVtkTools.txx
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2017-2025 OpenCFD Ltd.
+    Copyright (C) 2017-2026 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -41,22 +41,23 @@ template<class Face>
 vtkSmartPointer<vtkCellArray>
 Foam::vtk::Tools::Faces(const UList<Face>& faces)
 {
-    auto cells = vtkSmartPointer<vtkCellArray>::New();
-
-    #ifdef VTK_CELL_ARRAY_V2
+    #ifndef VTK_CELL_ARRAY_V2
+    #error "No support for defunct (VTK <= 8.2.0) vtkCellArray content"
+    return vtkSmartPointer<vtkCellArray>::New();
+    #else
 
     // Offsets
     // [0, n1, n1+n2, n1+n2+n3... ]
 
-    const vtkIdType nOffsets(faces.size()+1);
-
     auto offsets = vtkSmartPointer<vtkIdTypeArray>::New();
+
+    const vtkIdType nOffsets(faces.size()+1);
 
     vtkIdType nConnect(0);
     {
         offsets->SetNumberOfTuples(nOffsets);
 
-        vtkIdType* iter = offsets->WritePointer(0, nOffsets);
+        auto* iter = offsets->WritePointer(0, nOffsets);
 
         // Assign offsets, determine overall connectivity size
 
@@ -78,13 +79,13 @@ Foam::vtk::Tools::Faces(const UList<Face>& faces)
     {
         connect->SetNumberOfTuples(nConnect);
 
-        vtkIdType* iter = connect->WritePointer(0, nConnect);
+        auto* iter = connect->WritePointer(0, nConnect);
 
         // Fill in the connectivity array
 
         for (const auto& f : faces)
         {
-            for (const label verti : f)
+            for (auto verti : f)
             {
                 *(iter++) = verti;
             }
@@ -93,47 +94,12 @@ Foam::vtk::Tools::Faces(const UList<Face>& faces)
 
     // Move into a vtkCellArray
 
+    auto cells = vtkSmartPointer<vtkCellArray>::New();
+
     cells->SetData(offsets, connect);
 
-    #else
-
-    // In VTK-8.2.0 and older,
-    // sizes are interwoven (prefixed) in the connectivity
-
-    // Cell connectivity for polygons
-    // [n1, verts..., n2, verts... ]
-
-
-    const vtkIdType nElem(faces.size());
-
-    // Connectivity size, with prefixed size information
-    vtkIdType nConnect(faces.size());
-    for (const auto& f : faces)
-    {
-        nConnect += f.size();
-    }
-
-    {
-        cells->GetData()->SetNumberOfTuples(nConnect);
-
-        vtkIdType* iter = cells->WritePointer(nElem, nConnect);
-
-        // Fill in the connectivity array, with prefixed size information
-
-        for (const auto& f : faces)
-        {
-            *(iter++) = f.size();
-
-            for (const label verti : f)
-            {
-                *(iter++) = verti;
-            }
-        }
-    }
-
-    #endif
-
     return cells;
+    #endif
 }
 
 
@@ -231,7 +197,7 @@ Foam::vtk::Tools::Patch::faceCentres(const PatchType& p)
 
     if (p.hasFaceCentres())
     {
-        for (const point& pt : p.faceCentres())
+        for (const auto& pt : p.faceCentres())
         {
             vtkpoints->SetPoint(pointId++, pt.cdata());
         }

src/conversion/vtk/adaptor/foamVtkToolsI.H
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2017-2025 OpenCFD Ltd.
+    Copyright (C) 2017-2026 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -102,7 +102,7 @@ Foam::vtk::Tools::Points
     auto vtkpoints = vtk::Tools::NewPoints(addr.size());
 
     vtkIdType pointId = 0;
-    for (const label pointi : addr)
+    for (const auto pointi : addr)
     {
         const auto& p = pts[pointi];
         vtkpoints->SetPoint(pointId++, p.cdata());
@@ -170,15 +170,15 @@ Foam::vtk::Tools::identityVertices
     const vtkIdType size
 )
 {
-    auto cells = vtkSmartPointer<vtkCellArray>::New();
-
-    #ifdef VTK_CELL_ARRAY_V2
-
-    // Offsets
-    // [0, n1, n1+n2, n1+n2+n3... ]
+    #ifndef VTK_CELL_ARRAY_V2
+    #error "No support for defunct (VTK <= 8.2.0) vtkCellArray content"
+    return vtkSmartPointer<vtkCellArray>::New();
+    #else
 
     auto offsets = vtkSmartPointer<vtkIdTypeArray>::New();
 
+    // Offsets
+    // [0, n1, n1+n2, n1+n2+n3... ]
     {
         const vtkIdType nOffsets(size+1);
 
@@ -205,38 +205,11 @@ Foam::vtk::Tools::identityVertices
 
     // Move into a vtkCellArray
 
+    auto cells = vtkSmartPointer<vtkCellArray>::New();
     cells->SetData(offsets, connect);
 
-    #else
-
-    // In VTK-8.2.0 and older,
-    // sizes are interwoven (prefixed) in the connectivity
-
-    // Connectivity size, with prefixed size information
-
-    // Cell connectivity for vertex
-    // [size, ids.., size, ids...]  -> therefore  [1, id, 1, id, ...]
-
-    const vtkIdType nElem(size);
-    const vtkIdType nConnect(2*size);
-
-    {
-        cells->GetData()->SetNumberOfTuples(nConnect);
-
-        vtkIdType* iter = cells->WritePointer(nElem, nConnect);
-
-        // Fill in the connectivity array, with prefixed size information
-
-        for (vtkIdType id = 0; id < nElem; ++id)
-        {
-            *(iter++) = 1;
-            *(iter++) = id;
-        }
-    }
-
-    #endif
-
     return cells;
+    #endif
 }
 
 

src/conversion/vtk/adaptor/foamVtkVtuAdaptor.H
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2017-2025 OpenCFD Ltd.
+    Copyright (C) 2017-2026 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -46,7 +46,7 @@ SourceFiles
 #ifndef Foam_vtk_vtuAdaptor_H
 #define Foam_vtk_vtuAdaptor_H
 
-#include "fvMesh.H"
+#include "polyMesh.H"
 #include "volFieldsFwd.H"
 #include "foamVtkTools.H"
 #include "foamVtkMeshMaps.H"
@@ -107,7 +107,7 @@ public:
         //  Calling this automatically populates the sizing and vtk::Caching
         inline vtkSmartPointer<vtkUnstructuredGrid> internal
         (
-            const fvMesh& mesh,
+            const polyMesh& mesh,
             //! Request polyhedral decomposition
             const bool decompPoly = false
         );

src/conversion/vtk/adaptor/foamVtkVtuAdaptorI.H
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2016-2025 OpenCFD Ltd.
+    Copyright (C) 2016-2026 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -69,13 +69,13 @@ Foam::vtk::vtuAdaptor::points
 
     // Normal points
     vtkIdType pointId = 0;
-    for (const point& p : pts)
+    for (const auto& p : pts)
     {
         vtkpoints->SetPoint(pointId++, p.cdata());
     }
 
     // Cell centres
-    for (const label celli : addPoints)
+    for (const auto celli : addPoints)
     {
         const auto& p = cc[celli];
         vtkpoints->SetPoint(pointId++, p.cdata());
@@ -107,14 +107,14 @@ Foam::vtk::vtuAdaptor::points
 
     // Normal points
     vtkIdType pointId = 0;
-    for (const label pointi : pointMap)
+    for (const auto pointi : pointMap)
     {
         const auto& p = pts[pointi];
         vtkpoints->SetPoint(pointId++, p.cdata());
     }
 
     // Cell centres
-    for (const label celli : addPoints)
+    for (const auto celli : addPoints)
     {
         const auto& p = cc[celli];
         vtkpoints->SetPoint(pointId++, p.cdata());
@@ -127,18 +127,17 @@ Foam::vtk::vtuAdaptor::points
 inline vtkSmartPointer<vtkUnstructuredGrid>
 Foam::vtk::vtuAdaptor::internal
 (
-    const fvMesh& mesh,
+    const polyMesh& mesh,
     const bool decompPoly
 )
 {
-    const vtk::vtuSizing::contentType output
-    (
-        #ifdef VTK_CELL_ARRAY_V2
-        vtk::vtuSizing::contentType::INTERNAL2
-        #else
-        vtk::vtuSizing::contentType::INTERNAL1
-        #endif
-    );
+    #ifndef VTK_CELL_ARRAY_V2
+    #error "No support for defunct (VTK <= 8.2.0) vtkCellArray content"
+    return vtkSmartPointer<vtkUnstructuredGrid>::New();
+    #else
+
+    constexpr vtk::vtuSizing::contentType output =
+        vtk::vtuSizing::contentType::INTERNAL2;
 
     vtk::vtuSizing sizing(mesh, decompPoly);
 
@@ -171,26 +170,12 @@ Foam::vtk::vtuAdaptor::internal
         )
     );
 
-    #ifdef VTK_CELL_ARRAY_V2
-
     auto cellConnect = vtkSmartPointer<vtkIdTypeArray>::New();
 
     UList<vtkIdType> cellsUL
     (
         vtk::Tools::asUList(cellConnect, nConnect)
     );
-
-    #else
-
-    cells->GetData()->SetNumberOfTuples(sizing.nFieldCells());
-
-    UList<vtkIdType> cellsUL
-    (
-        cells->WritePointer(sizing.nFieldCells(), nConnect),
-        nConnect
-    );
-
-    #endif
 
     UList<vtkIdType> facesUL
     (
@@ -227,8 +212,6 @@ Foam::vtk::vtuAdaptor::internal
     //   for any additional points (ie, mesh cell-centres)
     vtkmesh->SetPoints(this->points(mesh));
 
-    #ifdef VTK_CELL_ARRAY_V2
-
     // Move into a vtkCellArray
     cells->SetData(cellOffsets, cellConnect);
 
@@ -244,20 +227,9 @@ Foam::vtk::vtuAdaptor::internal
     {
         vtkmesh->SetCells(cellTypes, cells, nullptr, nullptr);
     }
-    #else
-
-    if (facesUL.size())
-    {
-        vtkmesh->SetCells(cellTypes, cellOffsets, cells, faceLocations, faces);
-    }
-    else
-    {
-        vtkmesh->SetCells(cellTypes, cellOffsets, cells, nullptr, nullptr);
-    }
-
-    #endif
 
     return vtkmesh;
+    #endif
 }
 
 

src/fileFormats/vtk/part/foamVtuCells.H
@@ -259,7 +259,6 @@ public:
         //- The \c "offsets" into the connectivity list vertLabels()
         //  The content depends on the format:
         //  - XML : end offset
-        //  - INTERNAL1 : begin location
         //  - INTERNAL2 : begin/end offsets
         //  - HDF : begin/end offsets
         inline const labelList& vertOffsets() const noexcept;

src/fileFormats/vtk/part/foamVtuCells.cxx
@@ -205,7 +205,6 @@ void Foam::vtk::vtuCells::populateOutput(const polyMesh& mesh)
             break;
         }
 
-        case contentType::INTERNAL1 :
         case contentType::INTERNAL2 :
         {
             populateInternal

src/fileFormats/vtk/part/foamVtuSizing.H
@@ -66,16 +66,6 @@ Description
     \endtable
 
     \table
-        internal1 storage (VTK-8 and earlier)
-        \c types        | vtk cell type (1-255)
-        \c connectivity | nLabels and unique vertex labels used by the cell
-        \c location     | begin location for each of \c connectivity
-        \c faces        | face stream for polyhedral cells
-                        | [nFaces, nFace0Pts, id0,id1,..., nFace1Pts, id0,...]
-        \c facelocation | begin location for each of \c faces, with -1 for primitive cells
-    \endtable
-
-    \table
         internal2 storage (with VTK_CELL_ARRAY_V2)
         \c types        | vtk cell type (1-255)
         \c connectivity | unique vertex labels used by the cell
@@ -151,7 +141,6 @@ public:
     {
         LEGACY,     //!< Legacy VTK content
         XML,        //!< XML (VTU) content
-        INTERNAL1,  //!< Internal vtkUnstructuredGrid content (old)
         INTERNAL2,  //!< Internal vtkUnstructuredGrid content, VTK_CELL_ARRAY_V2
         HDF,        //!< Similar to INTERNAL2, but with VTKHDF specifics
     };
@@ -162,8 +151,7 @@ public:
         //! Cell connectivity (ALL)
         CELLS,
         //! Cell connectivity offsets:
-        //! end-offsets (XML), locations (INTERNAL1),
-        //! begin/end offsets (INTERNAL2, HDF)
+        //! end-offsets (XML), begin/end offsets (INTERNAL2, HDF)
         CELLS_OFFSETS,
         //! Polyhedral face-stream (XML, INTERNAL) or face vertices (HDF)
         FACES,
@@ -729,13 +717,6 @@ public:
         label sizeXml(slotType slot) const
         {
             return sizeOf(contentType::XML, slot);
-        }
-
-        //- The calculated size for vtk-internal storage of the specified slot
-        FOAM_DEPRECATED_FOR(2025-11, "sizeOf(INTERNAL1, ...)")
-        label sizeInternal1(slotType slot) const
-        {
-            return sizeOf(contentType::INTERNAL1, slot);
         }
 
         //- The calculated size for vtk-internal storage of the specified slot

src/fileFormats/vtk/part/foamVtuSizing.cxx
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2016-2025 OpenCFD Ltd.
+    Copyright (C) 2016-2026 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -95,7 +95,6 @@ void Foam::vtk::vtuSizing::checkSizes
         }
 
         case contentType::XML :
-        case contentType::INTERNAL1 :
         case contentType::INTERNAL2 :
         {
             // XML, INTERNAL uses connectivity/offset pairs
@@ -214,7 +213,6 @@ Foam::vtk::vtuSizing::dummyFaceOffsets
         }
 
         case contentType::XML :
-        case contentType::INTERNAL1 :
         case contentType::INTERNAL2 :
         {
             // Primitive cells: -1 placeholder
@@ -611,39 +609,6 @@ Foam::label Foam::vtk::vtuSizing::sizeOf
             break;
         }
 
-        case contentType::INTERNAL1:
-        {
-            switch (slot)
-            {
-                case slotType::CELLS:
-                    // Cell connectivity and extra cell centres,
-                    // with size-prefix per cell
-                    return (nVertLabels() + nAddVerts() + nFieldCells());
-                    break;
-
-                case slotType::CELLS_OFFSETS:
-                    // The begin location per cell connectivity
-                    return nFieldCells();
-                    break;
-
-                case slotType::FACES:
-                    // Face stream with various prefixing
-                    return nFaceLabels(output);
-                    break;
-
-                case slotType::FACES_OFFSETS:
-                    // The per-cell begin location of each face stream
-                    return hasPolyCells() ? nFieldCells() : 0;
-                    break;
-
-                // HDF only
-                case slotType::POLY_FACEIDS:
-                case slotType::POLY_FACEIDS_OFFSETS:
-                    break;
-            }
-            break;
-        }
-
         case contentType::INTERNAL2:
         {
             switch (slot)
@@ -1029,39 +994,6 @@ void renumberVerts_legacy
     }
 }
 
-
-template<class IntListType, class OffsetIntType>
-void renumberVerts_internal1
-(
-    IntListType& vertLabels,
-    const OffsetIntType pointOffset
-)
-{
-    if (!pointOffset)
-    {
-        return;
-    }
-
-    // INTERNAL1 vertLabels contains
-    // - connectivity
-    // [nLabels, vertex labels...]
-
-    auto iter = vertLabels.begin();
-    const auto last = vertLabels.end();
-
-    while (iter < last)
-    {
-        auto nLabels = *iter;  // nLabels (for this cell)
-        ++iter;
-
-        while (nLabels--)
-        {
-            *iter += pointOffset;
-            ++iter;
-        }
-    }
-}
-
 } // End anonymous namespace
 
 
@@ -1082,12 +1014,6 @@ void Foam::vtk::vtuSizing::renumberVertLabels
         case contentType::LEGACY :
         {
             renumberVerts_legacy(vertLabels, pointOffset);
-            break;
-        }
-
-        case contentType::INTERNAL1 :
-        {
-            renumberVerts_internal1(vertLabels, pointOffset);
             break;
         }
 
@@ -1192,7 +1118,7 @@ void Foam::vtk::vtuSizing::renumberFaceOffsets
 
         default :
         {
-            // XML, INTERNAL1, INTERNAL2, etc offsets
+            // XML, INTERNAL2, etc offsets
             // [-1, off1, off2, ... -1, ..]
 
             // HDF offsets

src/fileFormats/vtk/part/foamVtuSizingImpl.cxx
@@ -37,12 +37,10 @@ namespace
 {
 
 // Adjust \p vertOffset for all cells.
-// On input it contains the cell sizes, but for INTERNAL1 it also
-// contains an embedded size prefix.
+// On input it contains the cell sizes.
 //
 // On output, the cell connectivity offsets :
 // - XML format = end-offsets
-// - INTERNAL1 = begin-offsets
 // - INTERNAL2 = begin/end-offsets
 // - HDF = begin/end-offsets
 // .
@@ -63,28 +61,12 @@ void adjustCellOffsets
 
         case vtuSizing::contentType::XML :
         {
-            // Transform cell sizes (vertOffset) into begin offsets
+            // Transform cell sizes (vertOffset) into end offsets
 
             // vertOffset[0] already contains its size, leave untouched
             for (label i = 1; i < vertOffset.size(); ++i)
             {
                 vertOffset[i] += vertOffset[i-1];
-            }
-            break;
-        }
-
-        case vtuSizing::contentType::INTERNAL1 :
-        {
-            // Transform cell sizes (vertOffset) into begin offsets
-            {
-                IntType beg(0);
-
-                for (IntType& off : vertOffset)
-                {
-                    const IntType sz(off);
-                    off = beg;
-                    beg += 1 + sz;  // Additional 1 to skip embedded prefix
-                }
             }
             break;
         }
@@ -120,7 +102,6 @@ void adjustCellOffsets
 //
 // On output, the face connectivity offsets :
 // - XML format = end-offsets, with -1 placeholders
-// - INTERNAL1 = begin-offsets, with -1 placeholders
 // - INTERNAL2 = begin/end-offsets, with -1 placeholders
 // - HDF = begin/end-offsets
 // .
@@ -160,7 +141,6 @@ void adjustFaceOffsets
             break;
         }
 
-        case vtuSizing::contentType::INTERNAL1 :
         case vtuSizing::contentType::INTERNAL2 :
         {
             // Transform face sizes (faceOffset) into begin locations,
@@ -260,11 +240,7 @@ void Foam::vtk::vtuSizing::populateArrays
 
     // Are vertLabels prefixed with the size?
     // Also use as the size of the prefixed information
-    const int prefix =
-    (
-        output == contentType::LEGACY
-     || output == contentType::INTERNAL1
-    ) ? 1 : 0;
+    const int prefix = (output == contentType::LEGACY) ? 1 : 0;
 
 
     // Initialization
@@ -328,8 +304,11 @@ void Foam::vtk::vtuSizing::populateArrays
     // Placement of additional decomposed cells
     label nCellDecomp = mesh.nCells();
 
-    // Placement of additional point labels
-    label nPointDecomp = mesh.nPoints();
+    // Placement of additional point labels (apex of decomposed cells)
+    label apexVertLabel = mesh.nPoints();
+
+    // Count of additional point labels
+    label nAddPoints = 0;
 
     // Non-decomposed polyhedral are represented as a face-stream.
     // For legacy format, this stream replaces the normal connectivity
@@ -530,11 +509,8 @@ void Foam::vtk::vtuSizing::populateArrays
             // to avoid defining negative cells.
             // VTK may not care, but we'll do it anyhow for safety.
 
-            // Mapping from additional point to cell, and the new vertex from
-            // the cell-centre
-            const label newVertexLabel = nPointDecomp;
-
-            addPointsIds[nPointDecomp++] = celli;
+            // The cell-centre corresponding to the apexVertLabel
+            addPointsIds[nAddPoints] = celli;
 
             // Whether to insert cell in place of original or not.
             bool firstCell = true;
@@ -605,7 +581,7 @@ void Foam::vtk::vtuSizing::populateArrays
                     }
 
                     // The apex
-                    vertLabels[vrtLoc++] = newVertexLabel;
+                    vertLabels[vrtLoc++] = apexVertLabel;
                 }
 
                 for (const face& tria : faces3)
@@ -655,9 +631,13 @@ void Foam::vtk::vtuSizing::populateArrays
                     }
 
                     // The apex
-                    vertLabels[vrtLoc++] = newVertexLabel;
+                    vertLabels[vrtLoc++] = apexVertLabel;
                 }
             }
+
+            // Increment values
+            ++apexVertLabel;
+            ++nAddPoints;
         }
         else
         {
@@ -826,11 +806,7 @@ void Foam::vtk::vtuSizing::populateArrays
 
     // Are vertLabels prefixed with the size?
     // Also use as the size of the prefixed information
-    const int prefix =
-    (
-        output == contentType::LEGACY
-     || output == contentType::INTERNAL1
-    ) ? 1 : 0;
+    const int prefix = (output == contentType::LEGACY) ? 1 : 0;
 
 
     // Initialization