91765b4a0806ecb602b32519f9f797b896be8cb7
Mark Olesen <Mark.Olesen@keysight.com>
2026-04-01 12:26:18 +0200

    CONFIG: add etc-mingw/ configuration (default, and as an example)

---
+++ etc-mingw/prefs.sh
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------------------
+# Preferences for mingw cross-compiled (openSUSE)
+
+export FOAM_CONFIG_ETC="etc-mingw"
+
+export WM_COMPILER=Mingw
+export WM_MPLIB=msmpi-10.0
+
+unset WM_COMPILE_CONTROL
+
+# WM_MPLIB=none
+
+# ----------------------------------------------------------------------------
